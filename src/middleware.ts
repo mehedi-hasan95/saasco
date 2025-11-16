b93c7a49dd02a3d79fa1cb7c5727c155f981fae0
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2025-11-16 20:27:11 +0600

    remove all middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/site(.*)", "/api/uploadthing(.*)"]);
-
 export default clerkMiddleware(async (auth, req) => {
-  // Protect private routes
-  if (!isPublicRoute(req)) await auth.protect();
-
   return NextResponse.next();
 });
 
